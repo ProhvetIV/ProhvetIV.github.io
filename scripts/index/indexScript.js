a5b866d303fbc8452e8ace64039ceb14a071ba51
Iisak <iisakvirma@gmail.com>
2024-04-20 21:30:31 +0300

    login func

--- scripts/index/indexScript.js
+++ scripts/index/indexScript.js
@@ -3,14 +3,20 @@ export async function login() {
 	const emailOrUsername = document.getElementById("email-or-username").value;
 	const password = document.getElementById("password").value;
 
+	// Get query and creds for the GraphQL request.
+	// Encrypt credentials.
+	const { query, creds } = getQueryAndCreds(emailOrUsername, password);
 	const userCredentials = encrypt(emailOrUsername) + ":" + encrypt(password);
 
+	// Fetch the data.
 	try {
 		const response = await fetch("https://01.kood.tech/api/auth/signin", {
 			method: "POST",
 			headers: {
+				"Content-Type": "application/json",
 				Authorization: "Basic " + userCredentials,
 			},
+			body: JSON.stringify({ query, creds }),
 		});
 
 		console.log(response);
@@ -19,14 +25,57 @@ export async function login() {
 			throw new Error("Failed to fetch data");
 		}
 
-		const data = await response.json();
-		console.log(data);
+		const responseData = await response.json();
+		console.log(responseData);
+
+		const token = responseData.data.signin.token;
+		console.log("Token: ", token);
 	} catch (error) {
 		console.log(error);
 	}
 }
 
+// Encrypt data for transfer to GraphQL endpoint. Base64.
 function encrypt(input) {
 	const encryptedPW = btoa(input);
 	return encryptedPW;
+}
+
+// Checks whether username is email or not. If true, then email.
+function emailValidation(email) {
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return regex.test(email);
+}
+
+// Make the query string and differentiate between email and username.
+function getQueryAndCreds(emailOrUsername, password) {
+	// Query when user inputs email and password.
+	if (emailValidation(emailOrUsername)) {
+		let creds = {
+			email: emailOrUsername,
+			password: password,
+		};
+		query = `
+			mutation signin($email: String!, $password: String!) {
+				signin(email: $email, password: $password) {
+					token
+				}
+			}
+		`;
+		return { query, creds };
+	}
+
+	// Query when user inputs username and password.
+	let creds = {
+		username: emailOrUsername,
+		password: password,
+	};
+	let query = `
+		mutation signin($username: String!, $password: String!) {
+			signin(username: $username, password: $password) {
+				token
+			}
+		}
+	`;
+	return { query, creds };
 }
