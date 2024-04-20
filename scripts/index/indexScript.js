68d7a04ef056cf0596b54d63d5fa0c7a4cc4e473
Iisak <iisakvirma@gmail.com>
2024-04-20 15:02:24 +0300

    base fetch

--- scripts/index/indexScript.js
+++ scripts/index/indexScript.js
@@ -4,14 +4,20 @@ export async function login() {
 	const password = document.getElementById("password").value;
 
 	const userCredentials = emailOrUsername + ":" + encryptPassword(password);
+	console.log(userCredentials);
 
 	try {
 		const response = await fetch("https://01.kood.tech/api/auth/signin", {
 			method: "POST",
+			mode: "cors",
+			cache: "default",
+			credentials: "include",
 			headers: {
 				Authorization: "Basic " + userCredentials,
+				"Access-Control-Allow-Origin": "http://localhost:5500",
 			},
 		});
+
 		if (!response.ok) {
 			throw new Error("Failed to fetch data");
 		}
