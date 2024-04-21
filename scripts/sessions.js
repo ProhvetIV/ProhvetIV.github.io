716a9255fb3f2beefeb6168237735de64fdfaad5
Iisak <iisakvirma@gmail.com>
2024-04-21 22:37:40 +0300

    login token check

--- scripts/sessions.js
+++ scripts/sessions.js
@@ -23,6 +23,9 @@ export function checkSessionExpiration(address) {
 		window.location.href = "index.html";
 	}
 
+	const expires = token.expires;
+	console.log(expires);
+
 	const expirationTime = parseInt(JSON.parse(token)["expires"]);
 	console.log(expirationTime);
 
