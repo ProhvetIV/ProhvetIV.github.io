27f135c9b63a250e838868d125aa4b98eab62988
Iisak <iisakvirma@gmail.com>
2024-04-23 10:57:04 +0300

    graphInfo fetch

--- scripts/profile/profilePage.js
+++ scripts/profile/profilePage.js
@@ -3,7 +3,8 @@ import * as queries from "./queries.js";
 
 export async function profilePage() {
 	checkSessionExpiration("profile");
-	const user = await userInfo();
+	const uInfo = await userInfo();
+	const gInfo = await graphInfo();
 }
 
 async function userInfo() {
@@ -14,6 +15,27 @@ async function userInfo() {
 				Authorization: "Bearer: " + sessionStorage.getItem("JWT")["value"],
 			},
 			body: queries.userInfoQuery,
+		});
+
+		const data = await info.json();
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function graphInfo() {
+	const xpQ = queries.xpQuery;
+	const xpV = queries.xpVariables;
+
+	try {
+		const info = await fetch("https://01.kood.tech/api/graphql-engine/v1/graphql", {
+			method: "GET",
+			headers: {
+				Authorization: "Bearer " + sessionStorage.getItem("JWT")["value"],
+			},
+			body: JSON.stringify({ xpQ, xpV }),
 		});
 
 		const data = await info.json();
