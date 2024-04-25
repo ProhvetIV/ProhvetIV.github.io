3707a0638343dcb1fe6fa74bb058629a3079e74f
Iisak <iisakvirma@gmail.com>
2024-04-25 23:07:20 +0300

    charts background color white

--- scripts/profile/progress.js
+++ scripts/profile/progress.js
@@ -1,5 +1,59 @@
-function getProgress(progress) {}
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
 
-function placeProgress(progress) {}
+function placeProgress(progress) {
+	const progressChart = document.getElementById("progress-chart");
+	const svgNS = "http://www.w3.org/2000/svg";
 
-export { getProgress, placeProgress };
+	// Create chart grid. First X-axis, then Y-axis.
+	const gridX = document.createElementNS(svgNS, "g");
+	const lineX = document.createElementNS(svgNS, "line");
+	gridX.classList.add("grid", "x-grid");
+	lineX.setAttributeNS(null, "x1", 90);
+	lineX.setAttributeNS(null, "y1", 50);
+	lineX.setAttributeNS(null, "x2", 90);
+	lineX.setAttributeNS(null, "y2", 500);
+
+	const gridY = document.createElementNS(svgNS, "g");
+	const lineY = document.createElementNS(svgNS, "line");
+	gridY.classList.add("grid", "y-grid");
+	lineY.setAttributeNS(null, "x1", 90);
+	lineY.setAttributeNS(null, "y1", 500);
+	lineY.setAttributeNS(null, "x2", 900);
+	lineY.setAttributeNS(null, "y2", 500);
+
+	// Add months.
+	const gridXtext = document.createElementNS(svgNS, "g");
+	months.forEach((month, index) => {
+		let x = index * 65 + 100;
+		let y = 110;
+
+		const monthText = document.createElementNS(svgNS, "text");
+		monthText.setAttributeNS(null, "x", x);
+		monthText.setAttributeNS(null, "y", y);
+		monthText.textContent = month;
+	});
+
+	// Add numbers to y-axis.
+
+	// Append everything in order.
+	progressChart.appendChild(gridX);
+	progressChart.appendChild(gridY);
+	gridX.appendChild(lineX);
+	gridY.appendChild(lineY);
+	progressChart.appendChild(gridXtext);
+}
+
+export { placeProgress };
