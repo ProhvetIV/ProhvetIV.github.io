e0158fdec06f6f337ff6a75e78e9b4a59361d769
Iisak <iisakvirma@gmail.com>
2024-04-25 20:19:04 +0300

    pass fail chart

--- scripts/profile/passFail.js
+++ scripts/profile/passFail.js
@@ -132,19 +132,19 @@ function createFailChart(piscine, chartName) {
 	headerBar.classList.add("headerBar");
 
 	const nameText = document.createElementNS(svgNS, "text");
-	nameText.setAttributeNS(null, "x", 225);
+	nameText.setAttributeNS(null, "x", 1);
 	nameText.setAttributeNS(null, "y", 8);
 	nameText.setAttributeNS(null, "dy", ".35em");
 	nameText.textContent = "EXERCISE";
 
 	const failsText = document.createElementNS(svgNS, "text");
-	failsText.setAttributeNS(null, "x", 400);
+	failsText.setAttributeNS(null, "x", 150);
 	failsText.setAttributeNS(null, "y", 8);
 	failsText.setAttributeNS(null, "dy", ".35em");
 	failsText.textContent = "FAILS";
 
 	const ratioText = document.createElementNS(svgNS, "text");
-	ratioText.setAttributeNS(null, "x", 460);
+	ratioText.setAttributeNS(null, "x", 400);
 	ratioText.setAttributeNS(null, "y", 8);
 	ratioText.setAttributeNS(null, "dy", ".35em");
 	ratioText.textContent = "RATIO";
@@ -180,25 +180,26 @@ function createFailChart(piscine, chartName) {
 		const bar = document.createElementNS(svgNS, "g");
 		bar.classList.add("bar");
 
-		const rect = document.createElementNS(svgNS, "rect");
-		rect.setAttributeNS(null, "width", x + 1);
-		rect.setAttributeNS(null, "height", 18);
-		rect.setAttributeNS(null, "y", counter * 20 + 20);
-
 		const text = document.createElementNS(svgNS, "text");
-		text.setAttributeNS(null, "x", 220);
+		text.setAttributeNS(null, "x", 1);
 		text.setAttributeNS(null, "y", counter * 20 + 28);
 		text.setAttributeNS(null, "dy", ".35em");
 		text.textContent = `${key}`;
 
+		const rect = document.createElementNS(svgNS, "rect");
+		rect.setAttributeNS(null, "x", 151);
+		rect.setAttributeNS(null, "width", x + 1);
+		rect.setAttributeNS(null, "height", 18);
+		rect.setAttributeNS(null, "y", counter * 20 + 20);
+
 		const ftext = document.createElementNS(svgNS, "text");
-		ftext.setAttributeNS(null, "x", 400);
+		ftext.setAttributeNS(null, "x", 155 + x);
 		ftext.setAttributeNS(null, "y", counter * 20 + 28);
 		ftext.setAttributeNS(null, "dy", ".35em");
 		ftext.textContent = `${fails}`;
 
 		const rtext = document.createElementNS(svgNS, "text");
-		rtext.setAttributeNS(null, "x", 460);
+		rtext.setAttributeNS(null, "x", 400);
 		rtext.setAttributeNS(null, "y", counter * 20 + 28);
 		rtext.setAttributeNS(null, "dy", ".35em");
 		rtext.textContent = `${ratio}`;
@@ -208,8 +209,8 @@ function createFailChart(piscine, chartName) {
 		}
 
 		piscineChart.appendChild(bar);
-		bar.appendChild(rect);
 		bar.appendChild(text);
+		bar.appendChild(rect);
 		bar.appendChild(ftext);
 		bar.appendChild(rtext);
 
