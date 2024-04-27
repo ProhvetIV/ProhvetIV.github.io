94d8ae44de631b86dfea529d827eab061ffb2bdf
Iisak <iisakvirma@gmail.com>
2024-04-27 20:46:48 +0300

    audit pie chart

--- scripts/profile/auditPie.js
+++ scripts/profile/auditPie.js
@@ -2,17 +2,20 @@ export function placeAuditPie(ratio, up, down) {
 	const auditPieChart = document.getElementById("audit-chart");
 	const svgNS = "http://www.w3.org/2000/svg";
 
+	// Necessary variables.
+	const total = up + down;
+	const percentage = (down / total) * 100;
+	const strokeDashNumber = (total / 100) * percentage;
+
 	// Create the pie.
 	const pie = document.createElementNS(svgNS, "circle");
 	pie.classList.add("pie");
 	pie.setAttributeNS(null, "cx", 200);
 	pie.setAttributeNS(null, "cy", 200);
 	pie.setAttributeNS(null, "r", 100);
+	pie.style.strokeDasharray = `0, ${total}`;
 
 	// Make the colored section.
-	const total = up + down;
-	const percentage = (down / total) * 100;
-	const strokeDashNumber = (total / 100) * percentage;
 	pie.style.strokeDasharray = `${strokeDashNumber}, ${total}`;
 
 	// Append.
