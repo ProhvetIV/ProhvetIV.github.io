52f1dbd1091390a4882b74f9c06d4062c6c87c11
Iisak <iisakvirma@gmail.com>
2024-04-27 17:54:17 +0300

    progress chart

--- scripts/profile/progress.js
+++ scripts/profile/progress.js
@@ -145,7 +145,6 @@ function placeProgress(progress, xp) {
 		line.setAttributeNS(null, "stroke-width", "1.5");
 		line.setAttributeNS(null, "fill", "rgb(121,0,121)");
 		lines.appendChild(line);
-		console.log(line);
 	}
 
 	// Add bars for months
