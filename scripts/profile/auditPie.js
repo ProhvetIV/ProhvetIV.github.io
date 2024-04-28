42c4c8e7076cba7c052bf689045bc362359ccc4a
Iisak <iisakvirma@gmail.com>
2024-04-28 16:31:13 +0300

    audit pie chart

--- scripts/profile/auditPie.js
+++ scripts/profile/auditPie.js
@@ -2,10 +2,12 @@ export function placeAuditPie(ratio, up, down) {
 	const auditPieChart = document.getElementById("audit-chart");
 	const svgNS = "http://www.w3.org/2000/svg";
 
+	const circumference = 628;
+
 	// Necessary variables.
 	const total = up + down;
 	const percentage = (down / total) * 100;
-	const oneFiveEightTotal = (158 / 100) * percentage;
+	const oneFiveEightTotal = (circumference / 100) * percentage;
 	const strokeDashNumber = (total / 100) * percentage;
 
 	// Create the pie.
@@ -14,10 +16,10 @@ export function placeAuditPie(ratio, up, down) {
 	pie.setAttributeNS(null, "cx", 100);
 	pie.setAttributeNS(null, "cy", 100);
 	pie.setAttributeNS(null, "r", 100);
-	pie.style.strokeDasharray = 0 + " " + 158;
+	pie.style.strokeDasharray = 0 + " " + circumference;
 
 	// Make the colored section.
-	pie.style.strokeDasharray = `${oneFiveEightTotal}, ${158}`;
+	pie.style.strokeDasharray = `${oneFiveEightTotal}, ${circumference}`;
 
 	// Append.
 	auditPieChart.appendChild(pie);
